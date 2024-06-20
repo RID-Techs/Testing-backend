2b971fd2424e5d15d7283b85c2c492f1c0ab8d28
RID-Techs <bossbossa954@gmail.com>
2024-06-20 13:39:49 +0000

    Index modified

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port =process.env.PORT || 9003
 lunchDB() 
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://testing-front-ti8z.onrender.com",
     credentials: true
 }))
 
